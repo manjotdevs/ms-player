c05790074cac1bd9df9b79cf1e5dc94790615d8d
artix <you@example.com>
2024-03-22 22:01:03 +0530

    new images added

--- App.jsx
+++ App.jsx
@@ -1,4 +1,4 @@
-import { Text, View, SafeAreaView } from "react-native";
+import { StatusBar, View, SafeAreaView } from "react-native";
 import Navbar from "./components/Navbar";
 import Buttion1 from "./components/Button1";
 import Buttion2 from "./components/Button2";
@@ -6,8 +6,8 @@ import Mix_playlis from "./components/Mix_playlist";
 
 export default function App() {
   return (
-    <SafeAreaView className="pt-8 text-white bg-gray-950 h-full ">
-      <View>
+    <SafeAreaView className="pt-4 text-white bg-gray-950 h-full ">
+      <View className="pt-2">
         <View className="flex-row justify-items-start px-2 space-x-2">
           <View>
             <Buttion1 tittle="All" />
@@ -20,14 +20,14 @@ export default function App() {
           </View>
         </View>
         <SafeAreaView>
-          <View className="flex-row mt-5 ">
-            <View>
+          <View className="flex-row px-1 mt-5 ">
+            <View className="px-1">
               <Mix_playlis
                 image={require("./assets/Images/tarsem_jassar.jpg")}
-                tittle={`Tarsem Jassar`}
+                tittle={`Tarsem's\n Jassar`}
               />
               <Mix_playlis
-                image={require("./assets/Images/sidh_moose_wala.jpg")}
+                image={require("./assets/Images/sidh_moose_wala_mix.jpg")}
                 tittle={`Sidhu Moose\n Wala`}
               />
               <Mix_playlis
@@ -35,23 +35,24 @@ export default function App() {
                 tittle={`Daily Mix1`}
               />
             </View>
-            <View>
+            <View className="">
               <Mix_playlis
-                image={require("./assets/Images/sidh_moose_wala.jpg")}
-                tittle={`Tarsem Jassar`}
+                image={require("./assets/Images/new_music.jpg")}
+                tittle={`New Music`}
               />
               <Mix_playlis
-                image={require("./assets/Images/sidh_moose_wala.jpg")}
-                tittle={`Tarsem Jassar`}
+                image={require("./assets/Images/punjabi_101.jpg")}
+                tittle={`Punjabi 101`}
               />
               <Mix_playlis
-                image={require("./assets/Images/sidh_moose_wala.jpg")}
-                tittle={`Tarsem Jassar`}
+                image={require("./assets/Images/maga_punjabi_hits.jpg")}
+                tittle="Mega Punjabi "
               />
             </View>
           </View>
         </SafeAreaView>
         <Navbar />
+        <StatusBar backgroundColor="#1A202C" barStyle="light-content" />
       </View>
     </SafeAreaView>
   );
--- components/Mix_playlist.jsx
+++ components/Mix_playlist.jsx
@@ -3,8 +3,8 @@ import { View, Image, TouchableOpacity, Text } from "react-native";
 
 export default function Mix_playlist(props) {
   return (
-    <View className="pb-3 pr-2 pl-2">
-      <TouchableOpacity className="bg-gray-900 rounded-lg flex-row">
+    <View className=" pb-3 pr-1 pl-1">
+      <TouchableOpacity className="h-16 bg-gray-900 rounded-lg flex-row">
         <View className="h-16">
           <Image className="h-full w-16 rounded-l-lg" source={props.image} />
         </View>
